619598bbd6cccb1a59c6a76569c0e15f7159d328
Herwin Rosado <03host03@gmail.com>
2019-07-15 12:06:20 -0500

    Metodo Limpiar Completo y Asignacion de Valores a metodo Calcular

--- app/src/main/java/com/example/tallerandroid/MainActivity.java
+++ app/src/main/java/com/example/tallerandroid/MainActivity.java
@@ -2,15 +2,17 @@ package com.example.tallerandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView resultado;
+    Double num;
     EditText cant_Manillas;
     String materialManilla[];
     String tipoDije[];
@@ -18,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     Spinner comboMaterialManilla;
     Spinner comboTipoDije;
     Spinner comboTipoMaterialDije;
+    TextView resultado;
+    LinearLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         comboMaterialManilla = findViewById(R.id.cmbMaterial);
         comboTipoDije = findViewById(R.id.cmbTipoDije);
         comboTipoMaterialDije = findViewById(R.id.cmbMaterialDije);
+        layout = findViewById(R.id.layout_vertical);
 
         materialManilla = getResources().getStringArray(R.array.material);
         ArrayAdapter<String> adapterMaterial = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, materialManilla);
@@ -42,23 +47,42 @@ public class MainActivity extends AppCompatActivity {
         comboTipoMaterialDije.setAdapter(adapterTipoMaterialDije);
     }
 
-    public void visualizarLayout()
+    public void visualizarLayout(View v)
     {
-
+        layout.setVisibility(View.VISIBLE);
     }
 
     public void Limpiar()
     {
-
+        cant_Manillas.setText("");
+        comboMaterialManilla.setSelection(0);
+        comboTipoMaterialDije.setSelection(0);
+        comboTipoDije.setSelection(0);
+        resultado.setText("");
+        cant_Manillas.requestFocus();
+        layout.setVisibility(View.INVISIBLE);
     }
 
-    public void validar()
+    public boolean validar()
     {
-
+        return true;
     }
 
     public void calcular(View v)
     {
+        if(validar())
+        {
+            int opcComboMaterialManilla;
+            int opcComboTipoDije;
+            int opComboTipoMaterialDije;
+            num = Double.parseDouble(cant_Manillas.getText().toString());
+            opcComboMaterialManilla = comboMaterialManilla.getSelectedItemPosition();
+            opcComboTipoDije = comboTipoDije.getSelectedItemPosition();
+            opComboTipoMaterialDije = comboTipoMaterialDije.getSelectedItemPosition();
 
+
+
+            Limpiar();
+        }
     }
 }
